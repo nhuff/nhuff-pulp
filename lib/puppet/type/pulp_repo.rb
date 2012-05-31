de62e5a6d002e338b61471d3698b3d915757fd6d
nhuff <nrhuff@umn.edu>
2012-05-31 13:28:22 -0500

    Start a type and provider for a pulp repo

---
+++ lib/puppet/type/pulp_repo.rb
@@ -0,0 +1,40 @@
+Puppet::Type.newtype(:pulp_repo) do
+  @doc = "Manage a repo definition on a pulp server"
+
+  ensurable do
+    newvalue(:present) do
+      provider.create
+    end
+
+    newvalue(:absent) do
+      provider.destroy
+    end
+
+    defaultto :present
+  end
+
+  newparam(:name) do
+    desc "The id of the repository"
+    validate do |value|
+      unless value =~ /^\S+/
+        raise ArgumentError, "Repository id must not contain spaces"
+      end
+    end
+  end
+
+  newparam(:pulp_user) do
+    desc "Username to auth to pulp server as"
+  end
+
+  newparam(:pulp_pass) do
+    desc "Password to use for auth with pulp server"
+  end
+  
+  newproperty(:descr) do
+    desc "The display name of the repository"
+  end
+
+  newproperty(:feed) do
+    desc "Set the feed to pull from"
+  end
+end
