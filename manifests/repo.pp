57a3ab6ac9c13e2566a9b0a287d89314a02e4d91
Nathan Huff <nrhuff@umn.edu>
2013-01-22 09:38:11 -0600

    Change to v2 repo

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -2,17 +2,18 @@ class pulp::repo {
   case $::osfamily {
     'redhat': {
       $repourl = $::operatingsystem ? {
-        /(.ed.at|.ent..)/ => 'http://repos.fedorapeople.org/repos/pulp/pulp/v1/stable/$releasever/$basearch/',
-        'fedora'          => 'http://repos.fedorapeople.org/repos/pulp/pulp/v1/stable/fedora-$releasever/$basearch/',
+        /(.ed.at|.ent..)/ => 'http://repos.fedorapeople.org/repos/pulp/pulp/v2/stable/$releasever/$basearch/',
+        'fedora'          => 'http://repos.fedorapeople.org/repos/pulp/pulp/v2/stable/fedora-$releasever/$basearch/',
         default           => '',
       }
 
       yumrepo{'pulp':
-        name     => 'pulp-v1-stable',
-        descr    => 'Pulp v1',
+        name     => 'pulp-v2-stable',
+        descr    => 'Pulp v2',
         baseurl  => $repourl,
         enabled  => '1',
         gpgcheck => '0',
+        before   => Class['pulp::server::package'],
       }
     }
     default: {
